d66037533f007b143ee2d329aa2965671ea599b3
Electorch <electorchfagget@gmail.com>
2024-05-07 15:42:41 -0400

    Second Preggo commit

---
+++ Assets/Scripts/TamapreggochiManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TamapreggochiManager : MonoBehaviour
+{
+	[Tooltip("Mouseover")]
+	public string Notes = "What does this script do?";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
